ae96ec40739aaa2a4f0f7c36185265ed2e31efd8
Pedro-Kirsten <91696460+Pedro-Kirsten@users.noreply.github.com>
2023-06-17 13:37:45 -0300

    Delete src directory

--- src/main/java/com/produtos/apirest/repository/LojaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.produtos.apirest.repository;
-
-import com.produtos.apirest.models.Loja;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LojaRepository extends JpaRepository<Loja, Long>{
-    	Loja findById(long id);
-}
--- src/main/java/com/produtos/apirest/repository/PessoaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.produtos.apirest.repository;
-
-import com.produtos.apirest.models.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
-    	Pessoa findById(long id);
-}
